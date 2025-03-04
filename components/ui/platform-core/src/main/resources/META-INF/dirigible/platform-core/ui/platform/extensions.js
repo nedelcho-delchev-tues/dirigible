a3afa381f0b61de15225d10010cdb8ce8d132340
StanZGenchev <stan.z.genchev@gmail.com>
2025-03-04 16:13:53 +0200

    Fixed #4737

--- components/ui/platform-core/src/main/resources/META-INF/dirigible/platform-core/ui/platform/extensions.js
+++ components/ui/platform-core/src/main/resources/META-INF/dirigible/platform-core/ui/platform/extensions.js
@@ -9,49 +9,81 @@
  * SPDX-FileCopyrightText: Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-angular.module('platformExtensions', [])
-    /*
-     * The 'extensionPoints' constant can be used to replace the default extension points from your controller.
-     * Here is an example:
-     * .constant('extensionPoints', {
-     *     perspectives: ["example-perspectives"],
-     *     shells: ["example-shells"],
-     *     views: ["example-views"],
-     *     subviews: ["example-subviews"],
-     *     editors: ["example-editors"],
-     *     menus: ["example-menus"],
-     *     windows: ["example-windows"],
-     *     themes: ["example-themes"],
-     *     settings: ["example-settings"],
-     * })
-     */
-    .constant('extensionPoints', {})
-    .factory('Extensions', ['$http', 'extensionPoints', ($http, extensionPoints) => ({
-        getViews: (exPoints = extensionPoints.views) => {
-            return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints } });
-        },
-        getSubviews: (exPoints = extensionPoints.subviews) => {
-            return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints ?? ['platform-subviews'] } });
-        },
-        getWindows: (exPoints = extensionPoints.windows) => {
-            return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints ?? ['platform-windows'] } });
-        },
-        getSettings: (exPoints = extensionPoints.settings) => {
-            return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints ?? ['platform-settings'] } });
-        },
-        getEditors: (exPoints = extensionPoints.editors) => {
-            return $http.get('/services/js/platform-core/extension-services/editors.js', { params: { extensionPoints: exPoints } });
-        },
-        getPerspectives: (exPoints = extensionPoints.perspectives) => {
-            return $http.get('/services/js/platform-core/extension-services/perspectives.js', { params: { extensionPoints: exPoints } });
-        },
-        getShells: (exPoints = extensionPoints.shells) => {
-            return $http.get('/services/js/platform-core/extension-services/shells.js', { params: { extensionPoints: exPoints } });
-        },
-        getMenus: (exPoints = extensionPoints.menus) => {
-            return $http.get('/services/js/platform-core/extension-services/menus.js', { params: { extensionPoints: exPoints } });
-        },
-        getThemes: (exPoints = extensionPoints.themes) => {
-            return $http.get('/services/js/platform-core/extension-services/themes.js', { params: { extensionPoints: exPoints } });
-        },
-    })]);
+// @ts-nocheck
+if (!top.hasOwnProperty('PlatformExtPoints')) top.PlatformExtPoints = {
+    perspectives: ["platform-perspectives"],
+    shells: ["platform-shells"],
+    views: ["platform-views"],
+    subviews: ["platform-subviews"],
+    editors: ["platform-editors"],
+    menus: ["platform-menus"],
+    windows: ["platform-windows"],
+    themes: ["platform-themes"],
+    settings: ["platform-settings"],
+};
+function getExtPoints() {
+    if (top.hasOwnProperty('PlatformExtPoints')) return top.PlatformExtPoints;
+    throw Error("PlatformExtPoints is not set!");
+}
+function setExtPoints({ perspectives, shells, views, subviews, editors, menus, windows, themes, settings } = {}) {
+    if (perspectives) top.PlatformExtPoints.perspectives = perspectives;
+    if (shells) top.PlatformExtPoints.shells = shells;
+    if (views) top.PlatformExtPoints.views = views;
+    if (subviews) top.PlatformExtPoints.subviews = subviews;
+    if (editors) top.PlatformExtPoints.editors = editors;
+    if (menus) top.PlatformExtPoints.menus = menus;
+    if (windows) top.PlatformExtPoints.windows = windows;
+    if (themes) top.PlatformExtPoints.themes = themes;
+    if (settings) top.PlatformExtPoints.settings = settings;
+}
+function addExtPoints({ perspective, shell, view, subview, editor, menu, window, theme, setting } = {}) {
+    if (perspective && !top.PlatformExtPoints.perspectives.includes(perspective)) top.PlatformExtPoints.perspectives.push(perspective);
+    if (shell && !top.PlatformExtPoints.perspectives.includes(shell)) top.PlatformExtPoints.shells.push(shell);
+    if (view && !top.PlatformExtPoints.perspectives.includes(view)) top.PlatformExtPoints.views.push(view);
+    if (subview && !top.PlatformExtPoints.perspectives.includes(subview)) top.PlatformExtPoints.subviews.push(subview);
+    if (editor && !top.PlatformExtPoints.perspectives.includes(editor)) top.PlatformExtPoints.editors.push(editor);
+    if (menu && !top.PlatformExtPoints.perspectives.includes(menu)) top.PlatformExtPoints.menus.push(menu);
+    if (window && !top.PlatformExtPoints.perspectives.includes(window)) top.PlatformExtPoints.windows.push(window);
+    if (theme && !top.PlatformExtPoints.perspectives.includes(theme)) top.PlatformExtPoints.themes.push(theme);
+    if (setting && !top.PlatformExtPoints.perspectives.includes(setting)) top.PlatformExtPoints.settings.push(setting);
+}
+function removeExtPoints({ perspective, shell, view, subview, editor, menu, window, theme, setting } = {}) {
+    if (perspective && top.PlatformExtPoints.perspectives.includes(perspective)) top.PlatformExtPoints.perspectives.splice(top.PlatformExtPoints.perspectives.indexOf(perspective), 1);
+    if (shell && top.PlatformExtPoints.shells.includes(shell)) top.PlatformExtPoints.shells.splice(top.PlatformExtPoints.shells.indexOf(shell), 1);
+    if (view && top.PlatformExtPoints.views.includes(view)) top.PlatformExtPoints.views.splice(top.PlatformExtPoints.views.indexOf(view), 1);
+    if (subview && top.PlatformExtPoints.subviews.includes(subview)) top.PlatformExtPoints.subviews.splice(top.PlatformExtPoints.subviews.indexOf(subview), 1);
+    if (editor && top.PlatformExtPoints.editors.includes(editor)) top.PlatformExtPoints.editors.splice(top.PlatformExtPoints.editors.indexOf(editor), 1);
+    if (menu && top.PlatformExtPoints.menus.includes(menu)) top.PlatformExtPoints.menus.splice(top.PlatformExtPoints.menus.indexOf(menu), 1);
+    if (window && top.PlatformExtPoints.windows.includes(window)) top.PlatformExtPoints.windows.splice(top.PlatformExtPoints.windows.indexOf(window), 1);
+    if (theme && top.PlatformExtPoints.themes.includes(theme)) top.PlatformExtPoints.themes.splice(top.PlatformExtPoints.themes.indexOf(theme), 1);
+    if (setting && top.PlatformExtPoints.settings.includes(setting)) top.PlatformExtPoints.settings.splice(top.PlatformExtPoints.settings.indexOf(setting), 1);
+}
+angular.module('platformExtensions', []).factory('Extensions', ($http) => ({
+    getViews: (exPoints = top.PlatformExtPoints.views) => {
+        return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints } });
+    },
+    getSubviews: (exPoints = top.PlatformExtPoints.subviews) => {
+        return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints } });
+    },
+    getWindows: (exPoints = top.PlatformExtPoints.windows) => {
+        return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints } });
+    },
+    getSettings: (exPoints = top.PlatformExtPoints.settings) => {
+        return $http.get('/services/js/platform-core/extension-services/views.js', { params: { extensionPoints: exPoints } });
+    },
+    getEditors: (exPoints = top.PlatformExtPoints.editors) => {
+        return $http.get('/services/js/platform-core/extension-services/editors.js', { params: { extensionPoints: exPoints } });
+    },
+    getPerspectives: (exPoints = top.PlatformExtPoints.perspectives) => {
+        return $http.get('/services/js/platform-core/extension-services/perspectives.js', { params: { extensionPoints: exPoints } });
+    },
+    getShells: (exPoints = top.PlatformExtPoints.shells) => {
+        return $http.get('/services/js/platform-core/extension-services/shells.js', { params: { extensionPoints: exPoints } });
+    },
+    getMenus: (exPoints = top.PlatformExtPoints.menus) => {
+        return $http.get('/services/js/platform-core/extension-services/menus.js', { params: { extensionPoints: exPoints } });
+    },
+    getThemes: (exPoints = top.PlatformExtPoints.themes) => {
+        return $http.get('/services/js/platform-core/extension-services/themes.js', { params: { extensionPoints: exPoints } });
+    },
+}));
