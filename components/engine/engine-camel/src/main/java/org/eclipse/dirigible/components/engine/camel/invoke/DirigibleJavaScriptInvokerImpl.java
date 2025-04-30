46988f9074354055aad23c8143d7a0e685d6fb80
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-04-30 11:28:17 +0300

    Add support for async camel steps (#4930)
    * add support for async camel steps
    
    * formatting
    
    * enhance isPromise

--- components/engine/engine-camel/src/main/java/org/eclipse/dirigible/components/engine/camel/invoke/DirigibleJavaScriptInvokerImpl.java
+++ components/engine/engine-camel/src/main/java/org/eclipse/dirigible/components/engine/camel/invoke/DirigibleJavaScriptInvokerImpl.java
@@ -16,14 +16,20 @@ import org.apache.camel.spi.Synchronization;
 import org.eclipse.dirigible.components.engine.camel.components.DirigibleJavaScriptInvoker;
 import org.eclipse.dirigible.graalium.core.DirigibleJavascriptCodeRunner;
 import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.proxy.ProxyExecutable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 class DirigibleJavaScriptInvokerImpl implements DirigibleJavaScriptInvoker {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DirigibleJavaScriptInvokerImpl.class);
     private final ClassLoader currentClassLoader;
 
     @Autowired
@@ -79,20 +85,67 @@ class DirigibleJavaScriptInvokerImpl implements DirigibleJavaScriptInvoker {
      * @return the message
      */
     private Message unwrapCamelMessage(Value value) {
-        validateIntegrationMessage(value);
-        IntegrationMessage message = value.asHostObject();
+        IntegrationMessage message = exctractIntegrationMessage(value);
         return message.getCamelMessage();
     }
 
-    /**
-     * Validate integration message.
-     *
-     * @param value the value
-     */
-    private void validateIntegrationMessage(Value value) {
-        if (!value.isHostObject() || !(value.asHostObject() instanceof IntegrationMessage)) {
-            throw new IllegalArgumentException(
-                    "Unexpected return received from sdk/integrations::onMessage(). Expected return type: IntegrationMessage.");
+    private IntegrationMessage exctractIntegrationMessage(Value value) {
+        if (isIntegrationMessage(value)) {
+            LOGGER.debug("Returned value [{}] is of expected type [{}]", value, IntegrationMessage.class);
+            return value.asHostObject();
+        }
+
+        if (isPromise(value)) {
+            LOGGER.debug("Returned value [{}] is a promise.", value);
+
+            return executePromise(value);
+        }
+
+        throw new IllegalArgumentException("Unexpected return received from sdk/integrations::onMessage(). Expected return type ["
+                + IntegrationMessage.class + "] or a promise of [" + IntegrationMessage.class + "]");
+    }
+
+    private boolean isIntegrationMessage(Value value) {
+        return value.isHostObject() && (value.asHostObject() instanceof IntegrationMessage);
+    }
+
+    private boolean isPromise(Value value) {
+        return value.hasMember("then") && value.getMember("then")
+                                               .canExecute()
+                && getMetaObjectString(value).contains("function Promise");
+    }
+
+    private String getMetaObjectString(Value value) {
+        Value metaObject = value.getMetaObject();
+        String metaObjectString = metaObject.toString();
+        return null == metaObject ? "" : (metaObjectString == null ? "" : metaObjectString);
+    }
+
+    private IntegrationMessage executePromise(Value promise) {
+        CountDownLatch latch = new CountDownLatch(1);
+        AtomicReference<IntegrationMessage> resultRef = new AtomicReference<>();
+
+        promise.invokeMember("then", (ProxyExecutable) args -> {
+            Value result = args[0];
+            if (!isIntegrationMessage(result)) {
+                throw new IllegalArgumentException("Unexpected values is returned from promise [" + promise + "] Expected return type ["
+                        + IntegrationMessage.class + "]. Returned result [" + result + "]");
+            }
+            resultRef.set(result.asHostObject());
+            latch.countDown();
+            return null;
+        });
+
+        waitForPromiseToResolve(promise, latch);
+
+        return resultRef.get();
+    }
+
+    private void waitForPromiseToResolve(Value promise, CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("Failed to await for promise execution for: " + promise, e);
         }
     }
 
